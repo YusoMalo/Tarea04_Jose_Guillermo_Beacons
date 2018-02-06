f36cc62c6364d210bac8b9cfd1b8a327d7cb5b03
Yuso Malo <yusoazul77@hotmail.com>
2018-02-06 14:22:41 -0600

    Hola

--- app/src/main/java/mx/itesm/yuso/tarea04_jose_guillermo_beacons/BeaconsAct.java
+++ app/src/main/java/mx/itesm/yuso/tarea04_jose_guillermo_beacons/BeaconsAct.java
@@ -1,5 +1,6 @@
 package mx.itesm.yuso.tarea04_jose_guillermo_beacons;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
@@ -20,8 +22,9 @@ import java.util.UUID;
 public class BeaconsAct extends AppCompatActivity {
 
     private BeaconManager managerB;
-    private BeaconRegion region;
+    private BeaconRegion region, region2;
     private TextView text;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,25 +42,59 @@ public class BeaconsAct extends AppCompatActivity {
             }
         });
 
+        text = findViewById(R.id.et_beacon1);
+        image = findViewById(R.id.iv_beacon2);
         configurarBeacon();
     }
 
     private void configurarBeacon() {
         managerB = new BeaconManager(this);
-        region = new BeaconRegion("azul", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),  26891, 46664);
+        region = new BeaconRegion("azul", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),  30296, 46626);
+
+        region2 = new BeaconRegion("AzulA",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+                26891, 46664);
 
         final View  v = this.getWindow().getDecorView();
         managerB.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
             @Override
             public void onEnteredRegion(BeaconRegion beaconRegion, List<Beacon> beacons) {
-                v.setBackground(Color.);
+                for (Beacon beacon: beacons){
+                    if (beacon.getMinor() == 301296 && beacon.getMajor() == 46626){
+                        text.setText("Hola mami, arriba, abajo, lento, lento");
+                    }
+                    else if (beacon.getMinor() == 301296 && beacon.getMajor() == 46626){
+                        image.setImageResource(android.R.color.transparent);
+                    }
+                }
             }
+
+
 
             @Override
             public void onExitedRegion(BeaconRegion beaconRegion) {
-
+                for (Beacon beacon: beacons){
+                    if (beacon.getMinor() == 301296 && beacon.getMajor() == 46626){
+                        text.setText("Hola mami, arriba, abajo, lento, lento");
+                    }
+                    else if (beacon.getMinor() == 301296 && beacon.getMajor() == 46626){
+                        image.setImageResource(android.R.color.transparent);
+                    }
+                }
+                text.setText("");
             }
         });
+
+        /*managerB.setMonitoringListener(new BeaconManager.BeaconRangingListener() {
+            @Override
+            public void onBeaconsDiscovered(BeaconRegion beaconRegion, List<Beacon> beacons) {
+                for (Beacon beacon: beacons){
+                    if (beacon.getMinor() == 301296 && beacon.getMajor() == 46626){
+
+                    }
+                }
+            }
+        });*/
     }
 
     @Override
